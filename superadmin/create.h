bb5e71375bd08148cc825f4acf251ddf45fbd111
HaikalAska <haikalhazka@gmail.com>
2025-12-09 20:13:48 +0700

    superAdmin Done

--- superadmin/create.h
+++ superadmin/create.h
@@ -8,6 +8,11 @@
 
 int getStaffCount();
 static void inputID(char *id);
+int inputWithEsc(char *buffer, int maxLen);
+int inputTglWithEsc(char *buffer, int maxLen);
+int inputTelpWithEsc(char *buffer, int maxLen);
+int pwESC(char *pw, int col, int row);
+
 
 typedef struct {
     char username[50];
@@ -15,7 +20,7 @@ typedef struct {
     char id[100];
     char tgl[100];
     char gender[100];
-    int NOtlpn[50];
+    char notlpn[20];
 }staff;
 
 void create() {
@@ -38,15 +43,45 @@ void create() {
 
         staff data;
 
-    int count = getStaffCount() + 1;
+        int count = getStaffCount() + 1;
         sprintf(data.id, "STF%03d", count);
         clearArea(3, 14, 20);
         gotoxy(3,14); printf("ID Otomatis  : %s", data.id);
-        gotoxy(3,15); printf("Nama         : ");inputID(data.username);
-        gotoxy(3,16); printf("Kata Sandi   :");inputPassword(data.password,17, 16);
-        gotoxy(3,17); printf("Tgl Lahir    : ");scanf("%s", data.tgl);
-        gotoxy(3,18); printf("Kelamin      : ");scanf("%s", data.gender);
-        gotoxy(3,19); printf("No Telpon    : ");scanf("%d", &data.NOtlpn);
+
+        // Input Nama dengan deteksi ESC
+        gotoxy(3,15); printf("Nama         : ");
+        if (!inputWithEsc(data.username, 50)) {
+            fclose(fp);
+            return; // Keluar jika ESC
+        }
+
+        // Input Password dengan deteksi ESC
+        gotoxy(3,16); printf("Kata Sandi   : ");
+        if (!pwESC(data.password, 17, 16)) {
+            fclose(fp);
+            return;
+        }
+
+        // Input Tanggal Lahir dengan deteksi ESC
+        gotoxy(3,17); printf("Tgl Lahir    : ");
+        if (!inputTglWithEsc(data.tgl, 100)) {  // FIXED: Ganti dari inputTglWithEsc menjadi !inputTglWithEsc
+            fclose(fp);
+            return;
+        }
+
+        // Input Kelamin dengan deteksi ESC
+        gotoxy(3,18); printf("Kelamin      : ");
+        if (!inputWithEsc(data.gender, 100)) {
+            fclose(fp);
+            return;
+        }
+
+        // Input No Telepon dengan deteksi ESC
+        gotoxy(3,19); printf("No Telpon    : ");
+        if (!inputTelpWithEsc(data.notlpn, 20)) {
+            fclose(fp);
+            return;
+        }
 
         fwrite(&data, sizeof(staff), 1, fp);
         fclose(fp);
@@ -55,10 +90,18 @@ void create() {
         printf("Data berhasil dibuat!");
         gotoxy(3,22);
         printf("Tambah lagi? (y/n): ");
-        getchar();
-        n = getchar();
 
-    }while (n == 'y' || n == 'Y');
+        // Input y/n dengan deteksi ESC
+        while (1) {
+            n = _getch();
+            if (n == 27) return; // ESC keluar
+            if (n == 'y' || n == 'Y' || n == 'n' || n == 'N') {
+                printf("%c", n);
+                break;
+            }
+        }
+
+    } while (n == 'y' || n == 'Y');
 }
 
 int getStaffCount() {
@@ -71,4 +114,99 @@ int getStaffCount() {
 
     return size / sizeof(staff);
 }
+
+int inputWithEsc(char *buffer, int maxLen) {
+    int idx = 0;
+    while (1) {
+        char ch = _getch();
+
+        if (ch == 27) { // ESC
+            return 0; // Return 0 = user cancel
+        }
+        else if (ch == 13) { // ENTER
+            buffer[idx] = '\0';
+            return 1; // Return 1 = success
+        }
+        else if (ch == 8 && idx > 0) { // BACKSPACE
+            idx--;
+            printf("\b \b");
+        }
+        else if (ch >= 32 && ch <= 126 && idx < maxLen - 1) {
+            buffer[idx++] = ch;
+            printf("%c", ch);
+        }
+    }
+}
+
+int inputTglWithEsc(char *buffer, int maxLen) {
+    int idx = 0;
+    while (1) {
+        char ch = _getch();
+
+        if (ch == 27) { // ESC
+            return 0;  // Return 0 = user cancel
+        }
+        else if (ch == 13) { // ENTER
+            buffer[idx] = '\0';
+            return 1;  // Return 1 = success
+        }
+        else if (ch == 8 && idx > 0) { // BACKSPACE
+            idx--;
+            printf("\b \b");
+        }
+        else if (((ch >= '0' && ch <= '9') || ch == '-') && idx < maxLen - 1) {
+            buffer[idx++] = ch;
+            printf("%c", ch);
+        }
+    }
+}
+
+int inputTelpWithEsc(char *buffer, int maxLen) {
+    int idx = 0;
+    while (1) {
+        char ch = _getch();
+
+        if (ch == 27) { // ESC
+            return 0;  // Return 0 = user cancel
+        }
+        else if (ch == 13) { // ENTER
+            buffer[idx] = '\0';
+            return 1;  // Return 1 = success
+        }
+        else if (ch == 8 && idx > 0) { // BACKSPACE
+            idx--;
+            printf("\b \b");
+        }
+        else if (ch >= '0' && ch <= '9' && idx < maxLen - 1) {
+            buffer[idx++] = ch;
+            printf("%c", ch);
+        }
+    }
+}
+
+int pwESC(char *pw, int col, int row) {
+    int i = 0;
+    char ch;
+
+    while (1) {
+        ch = _getch();
+
+        if (ch == 27) { // ESC
+            return 0;       // Return 0 = user cancel
+        }
+        else if (ch == 13) { // ENTER
+            pw[i] = '\0';
+            return 1;       // Return 1 = success
+        }
+        else if (ch == 8 && i > 0) { // BACKSPACE
+            i--;
+            printf("\b \b");
+        }
+        else if (i < 49 && ch >= 32 && ch <= 126) {
+            pw[i++] = ch;
+            printf("*");
+        }
+    }
+}
+
 #endif
--- superadmin/delete.h
+++ superadmin/delete.h
@@ -2,26 +2,96 @@
 #define PROJEK_DELETE_H
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 #include "create.h"
 
 void delete(){
-FILE *fp, *sampah;
+    FILE *fp, *sampah;
     staff data;
-    char target[50];
+    staff staffList[100];
+    int count = 0;
+    int pilihan;
+    char konfirmasi;
     int found = 0;
 
+
+
+    fp = fopen("staff.dat","rb");
+    if (fp == NULL) {
+        gotoxy(3,13); printf("File tidak ditemukan!");
+        getchar();
+        return;
+    }
+
+    while (fread(&data, sizeof(staff), 1, fp) == 1) {
+        staffList[count] = data;
+        count++;
+    }
+    fclose(fp);
+
+    if (count == 0) {
+        gotoxy(3, 13); printf("Tidak ada data staff!");
+        getchar();
+        getchar();
+        return;
+    }
+
+    // Input pilihan nomor
+    gotoxy(3, 13);
+    printf("Pilih No Urut: ");
+    scanf("%d", &pilihan);
+
+    // Validasi pilihan
+    if (pilihan < 1 || pilihan > count) {
+        gotoxy(3, 14); printf("Pilihan tidak valid!");
+        getchar();
+        getchar();
+        return;
+    }
+
+    // Tampilkan detail data dalam box
+    staff terpilih = staffList[pilihan - 1];
+
+    clearscreen();
+    system("chcp 65001 > nul");
+    fillBackground(0x90);
+    printBorder(1, 1, 153, 43);
+    FrameYangTengah(31, 1, 43);
+    FrameYangHider(1,9,153);
+    tampilanlogin("GAMBARASCI.txt", 60, 3);
+    gotoxy(8,5); printf("Kelompok 5");
+
+    gotoxy(50, 14); printf("Pilih No Urut: %d", pilihan);
+
+    // Buat box
+    printBorder(49, 15, 45, 8);
+    gotoxy(50, 16);  printf("===================DATA====================");
+    gotoxy(50, 17);  printf("Nama        : %s", terpilih.username);
+    gotoxy(50, 18); printf("Password    : %s", terpilih.password);
+    gotoxy(50, 19); printf("Tgl Lahir   : %s", terpilih.tgl);
+    gotoxy(50, 20); printf("No Telpon   : %s", terpilih.notlpn);
+    gotoxy(50, 21); printf("Gender      : %s", terpilih.gender);
+
+    // Konfirmasi penghapusan
+    gotoxy(50, 23); printf("Yakin ingin menghapus data ini? (y/n): ");
+    scanf(" %c", &konfirmasi);
+
+    if (tolower(konfirmasi) != 'y') {
+        gotoxy(50, 24); printf("Penghapusan dibatalkan!");
+        getchar();
+        getchar();
+        return;
+    }
+
+    // Proses penghapusan
     fp = fopen("staff.dat","rb");
     sampah = fopen("sampah.dat","wb");
 
-    gotoxy(3,13); printf("user delete:");
-    scanf("%s", target);
-
     while (fread(&data, sizeof(staff), 1, fp) == 1) {
-        if (strcmp(data.id, target) != 0) {
+        if (strcmp(data.id, terpilih.id) != 0) {
             fwrite(&data, sizeof(staff), 1, sampah);
-        }
-        else {
+        } else {
             found = 1;
         }
     }
@@ -29,17 +99,16 @@ FILE *fp, *sampah;
     fclose(sampah);
 
     if (found) {
-        remove( "staff.dat");
+        remove("staff.dat");
         rename("sampah.dat", "staff.dat");
-        gotoxy(3,15); printf("User '%s' berhasil dihapus!", target);
+        gotoxy(50, 24); printf("Data '%s' berhasil dihapus!", terpilih.username);
     } else {
         remove("sampah.dat");
-        gotoxy(3,15); printf("Data tidak ditemukan!");
+        gotoxy(50, 24); printf("Terjadi kesalahan saat menghapus!");
     }
-    getchar();
-    getchar();
 
+    getchar();
+    getchar();
 }
-
 
 #endif
